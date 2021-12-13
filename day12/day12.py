ffbd96b3724f153cb04c9d1ca30fb8a319e02b30
TK <tkburis@gmail.com>
2021-12-13 12:48:31 +0700

    Optimisation/reformat

--- day12/day12.py
+++ day12/day12.py
@@ -5,7 +5,7 @@ from queue import Queue
 def part1(adj):
     q = Queue()
     q.put(('start', ['start']))
-    all_paths = []
+    all_paths = 0
     while (not q.empty()):
         curr = q.get()
         curr_node = curr[0]
@@ -13,21 +13,21 @@ def part1(adj):
         already_small_caves = set(x for x in curr_path if x.islower())
         for neighbour in adj[curr_node]:
             if neighbour == 'end':
-                all_paths.append(curr_path+['end'])
+                all_paths += 1
             elif neighbour != 'start' and neighbour not in already_small_caves:
                 q.put((neighbour, curr_path+[neighbour]))
-    print('Part 1:', len(all_paths))
+    print('Part 1:', all_paths)
 
 def part2(adj):
     q = Queue()
     q.put(('start', ['start']))
-    all_paths = []
+    all_paths = 0
     while (not q.empty()):
         curr = q.get()
         curr_node = curr[0]
         curr_path = curr[1]
 
-        can_twice = 2 not in set(curr_path.count(x) for x in set(curr_path) if x.islower())  # has a small cave already been visited twice?
+        can_twice = 2 not in set(curr_path.count(x) for x in set(curr_path) if x.islower())  # has a small cave NOT already been visited twice? (i.e. we can visit a small cave twice now)
         if can_twice:
             can_small_caves = set(x for x in adj[curr_node] if x.islower() and x != 'start')  # if not, then we can go to any small cave we ant
         else:
@@ -36,10 +36,10 @@ def part2(adj):
 
         for neighbour in adj[curr_node]:
             if neighbour == 'end':
-                all_paths.append(curr_path+['end'])
+                all_paths += 1
             elif neighbour in can_caves:
                 q.put((neighbour, curr_path+[neighbour]))
-    print('Part 2:', len(all_paths))
+    print('Part 2:', all_paths)
 
 def main():
     adj = {}
